9220b80d64134903ce80a5d177ee651fea3f5129
DoanLeThanhThao <thaodlt280990@tvu-onschool.edu.vn>
2025-11-22 16:33:02 +0700

    Add files via upload

---
+++ TrangChu.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/giaodiennguoidung.Master" AutoEventWireup="true" CodeBehind="TrangChu.aspx.cs" Inherits="aspnet_dk24ttc3_doanlethanhthao_datphongkhachsan.TrangChu" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<div class="trangchu">
+    <div class="panel panel-default">
+        <div class="panel-heading"><h4>Đặt phòng ngay hôm nay nhận nhiều ưu đãi hấp dẫn!</h4></div>
+        <div class="panel-body">
+        <%=TB%>
+        <div class="form-group timkiem">
+            <div class="col-sm-3">
+                <div class="input-group">
+                <asp:Label class="input-group-addon" runat="server" Text="Ngày nhận phòng"></asp:Label>
+                <asp:TextBox ID="txtNgayNhanPhong" class="form-control" type="date" placeholder="Ngày nhận phòng" runat="server" AutoPostBack="False"></asp:TextBox>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="input-group">
+                <asp:Label class="input-group-addon" runat="server" Text="Số lượng khách"></asp:Label>
+                <asp:DropDownList ID="ddlSoLuongKhach" class="form-control" runat="server"></asp:DropDownList>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="input-group">
+                <asp:Label class="input-group-addon" runat="server" Text="Tỉnh thành"></asp:Label>
+                <asp:DropDownList ID="ddlTinhThanh" class="form-control" runat="server"></asp:DropDownList>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="input-group">
+                <asp:Button ID="btnTimKiem" class="btn btn-primary" OnClick="btnTimKiem_Click" runat="server" Text="Tìm kiếm"/>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <asp:DataList ID="dlKhachSan" class="col-sm-12" runat="server" DataKeyField="IDKS" Font-Bold="False" Font-Italic="False" Font-Overline="False" Font-Strikeout="False" Font-Underline="False" RepeatColumns="3" RepeatDirection="Horizontal" Width="100%" OnItemCommand="dlKhachSan_ItemCommand">
+                <HeaderTemplate>
+                    <div class="dataListHeader">
+                        Điểm đến phổ biến được nhiều người yêu thích
+                    </div>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <div class="danhsach">
+                    <asp:Image ID="imgHinhAnhKS" runat="server"
+                                   ImageUrl='<%# Eval("HinhAnhKS") %>'
+                                   AlternateText='<%# Eval("TenKS") %>' />
+                        <div class="danhsach-ten ">
+                            <%# Eval("TenKS") %>
+                        </div>
+                        <div class="danhsach-diachi">
+                            Đ/c: <%# Eval("DiaChiKS") %>
+                        </div>
+                        <div class="danhsach-dienthoai">
+                            <%--Giá: <%# Eval("Price", "{0:C}") %>--%>
+                            SĐT: <%# Eval("DienThoaiKS") %>
+                        </div>
+                        <div class="danhsach-mota">
+                            Mô tả: <%# Eval("MoTaKS") %>
+                        </div>
+                        <asp:Button ID="btnDatPhong" class="btn btn-primary" runat="server" Text="Đặt phòng" CommandName="DatPhongKhachSan" CommandArgument='<%# Eval("IDKS") %>' />
+                    </div>
+                </ItemTemplate>
+                <AlternatingItemTemplate>
+                    <div class="danhsach">
+                        <asp:Image ID="imgHinhAnhKSALT" runat="server"
+                                   ImageUrl='<%# Eval("HinhAnhKS") %>'
+                                   AlternateText='<%# Eval("TenKS") %>' />
+                        <div class="danhsach-ten">
+                            <%# Eval("TenKS") %>
+                        </div>
+                        <div class="danhsach-diachi">
+                            Đ/c: <%# Eval("DiaChiKS") %>
+                        </div>
+                        <div class="danhsach-dienthoai">
+                            SĐT: <%# Eval("DienThoaiKS") %>
+                        </div>
+                        <div class="danhsach-mota">
+                            Mô tả: <%# Eval("MoTaKS") %>
+                        </div>
+                        <asp:Button ID="btnDatPhongALT" class="btn btn-primary" runat="server" Text="Đặt phòng" CommandName="DatPhongKhachSan" CommandArgument='<%# Eval("IDKS") %>' />
+                    </div>
+                </AlternatingItemTemplate>
+                <SeparatorTemplate>
+                    <hr class="dataListSeparator" />
+                </SeparatorTemplate>
+                <FooterTemplate>
+                    <div class="dataListFooter">
+                        Tổng số sản phẩm: <%# dlKhachSan.Items.Count %>
+                    </div>
+                </FooterTemplate>
+            </asp:DataList>
+        </div>
+        </div>
+    </div>
+</div>
+</asp:Content>
---
+++ TrangChu.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace aspnet_dk24ttc3_doanlethanhthao_datphongkhachsan
+{
+    public partial class TrangChu : System.Web.UI.Page
+    {
+        public string TB = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LayDDLTinhThanh();
+                LayDDLSoLuongKhach();
+                LayDLKhachSan();
+            }
+        }
+
+        public void LayDDLTinhThanh()
+        {
+            try
+            {
+                // Kết nối CSDL
+                string connectionString = ConfigurationManager.ConnectionStrings["datphongkhachsanketnoisql"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    // Lấy ddlTinhThanh
+                    string query = "SELECT DISTINCT TinhThanh FROM KhachSan";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        ddlTinhThanh.Items.Clear();
+                        ddlTinhThanh.Items.Add(new ListItem("-- Chọn Tỉnh thành --", ""));
+                        SqlDataAdapter da = new SqlDataAdapter(command);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        ddlTinhThanh.AppendDataBoundItems = true;
+                        ddlTinhThanh.DataSource = dt;
+                        ddlTinhThanh.DataTextField = "TinhThanh";
+                        ddlTinhThanh.DataValueField = "TinhThanh";
+                        ddlTinhThanh.DataBind();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi lấy dữ liệu Tỉnh Thành: {ex.Message}");
+            }
+        }
+
+        public void LayDDLSoLuongKhach()
+        {
+            try
+            {
+                // Tạo ddlSoLuongKhach
+                ddlSoLuongKhach.Items.Clear();
+                ddlSoLuongKhach.Items.Add(new ListItem("-- Chọn Số khách --", "0"));
+                ddlSoLuongKhach.Items.Add(new ListItem("1 Người", "1"));
+                ddlSoLuongKhach.Items.Add(new ListItem("2 Người", "2"));
+                ddlSoLuongKhach.Items.Add(new ListItem("3 Người", "3"));
+                ddlSoLuongKhach.Items.Add(new ListItem("4 Người", "4"));
+                ddlSoLuongKhach.Items.Add(new ListItem("5 Người", "5"));
+                ddlSoLuongKhach.Items.Add(new ListItem("6 Người", "6"));
+                ddlSoLuongKhach.Items.Add(new ListItem("7 Người", "7"));
+                ddlSoLuongKhach.Items.Add(new ListItem("8 Người", "8"));
+                ddlSoLuongKhach.Items.Add(new ListItem("9 Người", "9"));
+                ddlSoLuongKhach.Items.Add(new ListItem("10 Người", "10"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi lấy dữ liệu Số Lượng Khách: {ex.Message}");
+            }
+        }
+
+        public void LayDLKhachSan()
+        {
+            try
+            {
+                // Kết nối CSDL
+                string connectionString = ConfigurationManager.ConnectionStrings["datphongkhachsanketnoisql"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    // Lấy ddlKhachSan
+                    string query = "SELECT IDKS, TenKS, EmailKS, DienThoaiKS, DiaChiKS, TinhThanh, MoTaKS, HinhAnhKS, XepHang FROM KhachSan";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        SqlDataAdapter da = new SqlDataAdapter(command);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dlKhachSan.DataSource = dt;
+                        dlKhachSan.DataBind();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi lấy dữ liệu Khách Sạn: {ex.Message}");
+            }
+        }
+
+        protected void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kết nối CSDL
+                string connectionString = ConfigurationManager.ConnectionStrings["datphongkhachsanketnoisql"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    StringBuilder queryBuilder = new StringBuilder("SELECT KS.IDKS, KS.TenKS, KS.EmailKS, KS.DienThoaiKS, KS.DiaChiKS, KS.TinhThanh, KS.MoTaKS, KS.HinhAnhKS, KS.XepHang FROM KhachSan KS INNER JOIN Phong P ON KS.IDKS = P.IDKS INNER JOIN DatPhong DP ON P.IDPhong = DP.IDPhong WHERE 1=1");
+                    List<string> conditions = new List<string>();
+                    
+                    string NgayNhanPhong = txtNgayNhanPhong.Text;
+                    if (NgayNhanPhong != "")
+                    {
+                        conditions.Add("DP.NgayNhanPhong = @NgayNhanPhong");
+                    }
+                    int SoLuongKhach = int.Parse(ddlSoLuongKhach.SelectedValue);
+                    if (SoLuongKhach > 0)
+                    {
+                        conditions.Add("DP.SoLuongKhach = @SoLuongKhach");
+                    }
+                    string TinhThanh = ddlTinhThanh.SelectedItem.ToString();
+                    if (TinhThanh != "")
+                    {
+                        conditions.Add("KS.TinhThanh = @TinhThanh");
+                    }
+                    if (conditions.Count > 0)
+                    {
+                        queryBuilder.Append(" AND ");
+                        queryBuilder.Append(string.Join(" AND ", conditions));
+                    }
+                    string query = queryBuilder.ToString();
+                    TB = query;
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (NgayNhanPhong != "")
+                        {
+                            command.Parameters.AddWithValue("@NgayNhanPhong", NgayNhanPhong);
+                        }
+                        if (SoLuongKhach > 0)
+                        {
+                            command.Parameters.AddWithValue("@SoLuongKhach", SoLuongKhach);
+                        }
+                        if (TinhThanh != "")
+                        {
+                            command.Parameters.AddWithValue("@TinhThanh", TinhThanh);
+                        }
+                        SqlDataAdapter da = new SqlDataAdapter(command);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        dlKhachSan.DataSource = dt;
+                        dlKhachSan.DataBind();
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi lấy dữ liệu Khách Sạn theo điều kiện: {ex.Message}");
+            }
+        }
+
+        protected void dlKhachSan_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if (e.CommandName == "DatPhongKhachSan")
+            {
+                string IDKS = dlKhachSan.DataKeys[e.Item.ItemIndex].ToString();
+                Response.Redirect("/DatPhong.aspx?IDKS=" + IDKS);
+            }
+        }
+    }
+}
